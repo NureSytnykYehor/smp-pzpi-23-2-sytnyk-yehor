7b1559ae2cf777474e288f192643c6b741364f00
Ситник Єгор <yehor.sytnyk@nure.ua>
2025-05-29 14:29:55 +0300

    feat(app, db, settings): implemented settings branch

--- Lab2/smp-pzpi-23-2-sytnyk-yehor-lab2/pzpi-23-2-sytnyk-yehor-task3.php
+++ Lab2/smp-pzpi-23-2-sytnyk-yehor-lab2/pzpi-23-2-sytnyk-yehor-task3.php
@@ -33,6 +33,43 @@ class DB
         } catch (PDOException $e) {
             throw new DbException("Connection to DB failed.\nCaused by: " . $e->getMessage());
         }
+
+        try {
+            $this->pdo->exec("
+                CREATE TABLE IF NOT EXISTS settings (
+                    name TEXT,
+                    age TEXT
+                );
+            ");
+
+            if ($this->pdo->query("SELECT COUNT(*) FROM settings;")->fetchColumn() == 0) {
+                $this->pdo->exec("INSERT INTO settings (name, age) VALUES ('user', 0);");
+            }
+        } catch (PDOException $e) {
+            throw new DbException("Error initialising settings table.\nCaused by: " . $e->getMessage());
+        }
+    }
+
+    /**
+     * Updates user information in the database
+     *
+     * @param string $name
+     * @param int $age
+     * @return void
+     * @throws DbException If there's a database error.
+     */
+    public function update_user($name, $age): void
+    {
+        try {
+            $stmt = $this->pdo->prepare("UPDATE settings SET name = :name, age = :age;");
+
+            $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+            $stmt->bindParam(':age', $age, PDO::PARAM_INT);
+
+            $stmt->execute();
+        } catch (PDOException $e) {
+            throw new DbException("Error updating user info.\nCaused by: " . $e->getMessage());
+        }
     }
 }
 
@@ -135,6 +172,32 @@ class App
     }
     private function settings(): void
     {
+        while (true) {
+            $name = readline("Ваше ім'я: ");
+            if ($name !== "" && preg_match("/[a-zA-Z]+/", $name))
+                break;
+        }
+
+        while (true) {
+            $age = readline("Ваш вік: ");
+
+            if (!filter_var($age, FILTER_VALIDATE_INT)) {
+                echo "ПОМИЛКА! Вік користувача потрібно вказати числом\n\n";
+                continue;
+            }
+
+            if ($age < 7 || $age > 150) {
+                echo "ПОМИЛКА! Користувач повинен мати вік від 7 та до 150 років\n\n";
+                continue;
+            }
+
+            break;
+        }
+
+        echo "\n";
+
+        $this->db->update_user($name, $age);
+
         $this->state = State::Menu;
     }
 
