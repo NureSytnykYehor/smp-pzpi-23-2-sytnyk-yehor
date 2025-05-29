0ef4a14af1dc6c32e6560456f0955eb666947fee
Ситник Єгор <yehor.sytnyk@nure.ua>
2025-05-29 14:33:24 +0300

    feat(app, db, items): implemented items branch

--- Lab2/smp-pzpi-23-2-sytnyk-yehor-lab2/pzpi-23-2-sytnyk-yehor-task3.php
+++ Lab2/smp-pzpi-23-2-sytnyk-yehor-lab2/pzpi-23-2-sytnyk-yehor-task3.php
@@ -48,6 +48,42 @@ class DB
         } catch (PDOException $e) {
             throw new DbException("Error initialising settings table.\nCaused by: " . $e->getMessage());
         }
+
+        try {
+            $this->pdo->exec("
+                CREATE TABLE IF NOT EXISTS items (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    price REAL NOT NULL
+                );
+            ");
+
+            if ($this->pdo->query("SELECT COUNT(id) FROM items;")->fetchColumn() == 0) {
+                $this->pdo->exec("
+                    INSERT INTO items (name, price) VALUES ('Молоко пастеризоване', 12);
+                    INSERT INTO items (name, price) VALUES ('Хліб чорний', 9);
+                    INSERT INTO items (name, price) VALUES ('Сир білий', 21);
+                    INSERT INTO items (name, price) VALUES ('Сметана 20%', 25);
+                    INSERT INTO items (name, price) VALUES ('Кефір 1%', 19);
+                    INSERT INTO items (name, price) VALUES ('Вода газована', 18);
+                    INSERT INTO items (name, price) VALUES ('Печиво \"Весна\"', 14);
+                ");
+            }
+        } catch (PDOException $e) {
+            throw new DbException("Error initialising items table.\nCaused by: " . $e->getMessage());
+        }
+
+        try {
+            $this->pdo->exec("
+                CREATE TABLE IF NOT EXISTS cart (
+                    id INTEGER NOT NULL UNIQUE,
+                    count INTEGER NOT NULL,
+                    FOREIGN KEY(id) REFERENCES item(id)
+                );
+            ");
+        } catch (PDOException $e) {
+            throw new DbException("Error initialising cart table.\nCaused by: " . $e->getMessage());
+        }
     }
 
     /**
@@ -69,6 +105,88 @@ class DB
             $stmt->execute();
         } catch (PDOException $e) {
             throw new DbException("Error updating user info.\nCaused by: " . $e->getMessage());
+        }
+    }
+
+    /**
+     * Fetches all items from the database.
+     *
+     * @return array[]
+     * @throws DbException If there's a database error.
+     */
+    public function get_items(): array
+    {
+        try {
+            $stmt = $this->pdo->query("SELECT id, name, price FROM items ORDER BY id;");
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw new DbException("Error retrieving data from the items table.\nCaused by: " . $e->getMessage());
+        }
+    }
+
+    /**
+     * Fetches all items in the cart from the database without price info.
+     *
+     * @return array[]
+     * @throws DbException If there's a database error.
+     */
+    public function get_cart_no_price(): array
+    {
+        try {
+            $stmt = $this->pdo->query(
+                "SELECT name, count FROM cart
+                INNER JOIN items ON cart.id = items.id
+                ORDER BY cart.id;"
+            );
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw new DbException("Error inserting init data in the items table.\nCaused by: " . $e->getMessage());
+        }
+    }
+
+    /**
+     * Add item to the cart
+     *
+     * @param int $id
+     * @param int $count
+     *
+     * @return bool
+     * @throws DbException If there's a database error.
+     */
+    public function add_to_cart($id, $count): bool
+    {
+        try {
+            $stmt = $this->pdo->prepare(
+                "INSERT INTO cart
+                    (id, count)
+                VALUES
+                    (:id, :count)
+                ON CONFLICT(id)
+                    DO UPDATE SET
+                        count = :count
+                WHERE id = :id;"
+            );
+            return $stmt->execute(['id' => $id, 'count' => $count]);
+        } catch (PDOException $e) {
+            throw new DbException("Error adding item to the cart.\nCaused by: " . $e->getMessage());
+        }
+    }
+
+    /**
+     * Remove an item from the cart
+     *
+     * @param int $id
+     *
+     * @return bool
+     * @throws DbException If there's a database error.
+     */
+    public function remove_from_cart($id): bool
+    {
+        try {
+            $stmt = $this->pdo->prepare("DELETE FROM cart WHERE id = :id");
+            return $stmt->execute(['id' => $id]);
+        } catch (PDOException $e) {
+            throw new DbException("Error removimg item from the cart.\nCaused by: " . $e->getMessage());
         }
     }
 }
@@ -164,6 +282,67 @@ class App
     }
     private function items(): void
     {
+        $items = $this->db->get_items();
+        array_unshift($items, ['id' => "№", 'name' => "НАЗВА", 'price' => "ЦІНА"]);
+        array_push($items, ['id' => " ", 'name' => "-----------", 'price' => ""]);
+        array_push($items, ['id' => "0", 'name' => "ПОВЕРНУТИСЯ", 'price' => ""]);
+        $columns = $this->count_columns($items);
+
+        while (true) {
+            $this->print_lits($items, $columns);
+
+            $id = readline("Виберіть товар: ");
+
+            if ($id == '0') {
+                break;
+            }
+
+            echo "\n";
+
+            $selected = null;
+            foreach ($items as $item) {
+                if ($item['id'] === (int)$id)
+                    $selected = $item;
+            }
+
+            if ($selected == null) {
+                echo "ПОМИЛКА! ВКАЗАНО НЕПРАВИЛЬНИЙ НОМЕР ТОВАРУ\n";
+                continue;
+            }
+
+            echo "Вибрано: {$selected['name']}\n";
+
+            $count = (int)readline("Введіть кількість, штук: ");
+
+            if ($count > 100) {
+                echo "ПОМИЛКА! Не можна додати більше 100 одиниць товару в кошик\n";
+                continue;
+            }
+
+            if ($count < 0) {
+                echo "ПОМИЛКА! Кількість не може бути від'ємною\n";
+                continue;
+            }
+
+            if ($count == 0) {
+                echo "ВИДАЛЯЮ З КОШИКА\n";
+                $this->db->remove_from_cart($id);
+            } else {
+                $this->db->add_to_cart($id, $count);
+            }
+
+            $cart = $this->db->get_cart_no_price();
+            if (count($cart) == 0) {
+                echo "КОШИК ПОРОЖНІЙ\n";
+            } else {
+                echo "\nУ КОШИКУ:\n";
+                array_unshift($cart, ['name' => "НАЗВА", 'count' => "КІЛЬКІСТЬ"]);
+                $cart_columns = $this->count_columns($cart);
+                $this->print_lits($cart, $cart_columns);
+                echo "\n";
+            }
+        }
+
         $this->state = State::Menu;
     }
     private function checkout(): void
@@ -200,6 +379,47 @@ class App
 
         $this->state = State::Menu;
     }
+
+    /**
+     * @param array<array> $items
+     * @return array<int>
+     */
+    private function count_columns($items): array
+    {
+        $columns = [];
+        foreach ($items as $item) {
+            foreach ($item as $field => $value) {
+                if (!key_exists($field, $columns))
+                    $columns[$field] = mb_strlen($value);
+                else
+                    $columns[$field] = max(mb_strlen($value), $columns[$field]);
+            }
+        }
+
+        return $columns;
+    }
+    /**
+     * @param array $element
+     * @param array<int> $columns
+     */
+    private function pad_row($element, $columns): string
+    {
+        $result = [];
+        foreach ($element as $field => $value)
+            $result[] = mb_str_pad($value, $columns[$field], ' ', STR_PAD_RIGHT);
+
+        return implode("  ", $result);
+    }
+    /**
+     * @param array<array> $items
+     * @param array<int> $columns
+     */
+    private function print_lits($items, $columns): void
+    {
+        foreach ($items as $item)
+            echo $this->pad_row($item, $columns) . "\n";
+    }
+}
 
 try {
     $app = new App("db.sqlite");
